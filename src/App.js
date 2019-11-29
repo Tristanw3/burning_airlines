1e12645ea32921644afa0753f529bc48ad42b46f
Tristan <tristanw3@outlook.com>
2019-11-30 09:41:32 +1100

    test

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
-import React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Navbar from "./Navbar/Navbar";
-import Login from "./Login/Login";
+// Import components
+import Navbar from './Navbar/Navbar';
+import Login from './Login/Login';
 
-import Flights from "./Flights/Flights";
-import Airplanes from "./Airplanes/Airplanes";
+import Flights from './Flights/Flights';
+import Airplanes from './Airplanes/Airplanes';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { render } from "react-dom"
-import { Router, Link } from "@reach/router"
-
-
-
+import { render } from 'react-dom';
+import { Router, Link } from '@reach/router';
 
 class App extends React.Component {
-  state = {};
+	state = {};
 
+	render() {
+		return (
+			<div className="App">
+				<Navbar />
 
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-
-        <Router>
-
-          <Login path="/" />
-          <Flights path="/flights" />
-          <Airplanes path="/planes" />
-
-        </Router>
-      </div>
-    );
-  }
+				<Router>
+					<Login path="/" />
+					<Flights path="/flights" />
+					<Airplanes path="/planes" />
+				</Router>
+			</div>
+		);
+	}
 }
-
 
 export default App;
