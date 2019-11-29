63f102242b499f9aece3e2e3abc7a3a6dd1834d8
Tristan <tristanw3@outlook.com>
2019-11-29 22:23:23 +1100

    add more

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Navbar.css";
+
 class Navbar extends React.Component {
   state = {};
   render() {
     return (
       <div>
         <nav>
+          <h1>GA Airlines</h1>
           <a href="./">Home </a>
-          <a href="./airplanes">Airplanes</a>
+          <a href="./planes">Airplanes</a>
           <a href="./flights">Flights</a>
 
         </nav>
